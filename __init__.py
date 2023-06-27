68f35e7a5a3d3a5301ed30ff5a7d353f628bd8bd
gingmzmzx <2417481092@qq.com>
2023-06-27 17:17:32 +0800

    阿巴阿巴阿巴阿巴

--- __init__.py
+++ __init__.py
@@ -1,6 +1,11 @@
-from pbf.controller.PBF import PBF
-from pbf.utils.RegCmd import RegCmd
 import time
+
+from pbf.controller.PBF import PBF
+from pbf.controller import Cache
+from pbf.model.BlackListModel import BlackListModel
+from pbf.model.BanWordsModel import BanWordsModel
+from pbf.model import ModelBase
+from pbf.utils.RegCmd import RegCmd
 
 _name = "黑白系统"
 _version = "1.0.1"
@@ -8,93 +13,92 @@ _description = "黑白系统，包括全局屏蔽、违禁词系统"
 _author = "xzyStudio"
 _cost = 0.00
 
+
 class blacklist(PBF):
     @RegCmd(
-        name = "加违禁词 ",
-        usage = "加违禁词 <违禁词内容>",
-        permission = "anyone",
-        description = "添加违禁词",
-        mode = "违禁系统"
+        name="加违禁词 ",
+        usage="加违禁词 <违禁词内容>",
+        permission="anyone",
+        description="添加违禁词",
+        mode="违禁系统"
     )
     def addWeijin(self):
         uid = self.data.se.get('user_id')
         gid = self.data.se.get('group_id')
         message = self.data.args[1]
-        
+
         if self.data.args[-1] == "单群":
-            self.mysql.commonx('INSERT INTO `botWeijin` (`content`, `state`, `qn`) VALUES (%s, 0, %s)', (message, self.data.se.get("group_id")))
+            BanWordsModel()._insert(content=message, state=0, qn=self.data.se.get("group_id"))
         else:
-            if uid == self.data.botSettings.get('owner'):
-                self.mysql.commonx('INSERT INTO `botWeijin` (`content`, `state`) VALUES (%s, 0)', (message))
+            if uid == self.data.botSettings._get('owner'):
+                BanWordsModel()._insert(content=message, state=0)
             else:
-                self.mysql.commonx('INSERT INTO `botWeijin` (`content`, `state`, `qn`) VALUES (%s, 1, 0)', (message))
+                BanWordsModel()._insert(content=message, state=1, qn=0)
         self.client.msg().raw('[CQ:face,id=54] 插入成功，等待审核！')
-        
-        Cache.refreshFromSql('botWeijin')
-    
+
     @RegCmd(
-        name = "违禁词垃圾箱",
-        usage = "违禁词垃圾箱",
-        permission = "owner",
-        description = "查看违禁词垃圾箱",
-        mode = "违禁系统"
+        name="违禁词垃圾箱",
+        usage="违禁词垃圾箱",
+        permission="owner",
+        description="查看违禁词垃圾箱",
+        mode="违禁系统"
     )
     def bWj(self):
         uid = self.data.se.get('user_id')
         gid = self.data.se.get('group_id')
-        
-        vKwList = self.mysql.selectx('SELECT * FROM `botWeijin` WHERE `state`=2')
-        message = '[CQ:face,id=151] {0}-违禁词垃圾箱'.format(self.data.botSettings.get('name'))
+
+        vKwList = BanWordsModel()._get(state=2)
+        message = '[CQ:face,id=151] {0}-违禁词垃圾箱'.format(self.data.botSettings._get('name'))
         for i in vKwList:
-            message += '\n[CQ:face,id=54] 违禁词：'+str(i.get('content'))+'\n      ID：'+str(i.get('id'))
+            message += '\n[CQ:face,id=54] 违禁词：' + str(i.get('content')) + '\n      ID：' + str(i.get('id'))
         self.client.msg().raw(message)
-    
+
     @RegCmd(
-        name = "违禁词审查列表",
-        usage = "违禁词审查列表",
-        permission = "owner",
-        description = "查看违禁词审核列表",
-        mode = "违禁系统"
+        name="违禁词审查列表",
+        usage="违禁词审查列表",
+        permission="owner",
+        description="查看违禁词审核列表",
+        mode="违禁系统"
     )
     def vWj(self):
         uid = self.data.se.get('user_id')
         gid = self.data.se.get('group_id')
-        
-        vKwList = self.mysql.selectx('SELECT * FROM `botWeijin` WHERE `state`=1')
-        message = '[CQ:face,id=151] {0}-违禁词审核列表'.format(self.data.botSettings.get('name'))
+
+        vKwList = BanWordsModel()._get(state=1)
+        message = '[CQ:face,id=151] {0}-违禁词审核列表'.format(self.data.botSettings._get('name'))
         for i in vKwList:
-            message += '\n[CQ:face,id=54] 违禁词：'+str(i.get('content'))+'\n      ID：'+str(i.get('id'))
+            message += '\n[CQ:face,id=54] 违禁词：' + str(i.get('content')) + '\n      ID：' + str(i.get('id'))
         self.client.msg().raw(message)
-    
+
     @RegCmd(
-        name = "违禁词删除列表",
-        usage = "违禁词删除列表",
-        permission = "owner",
-        description = "查看违禁词删除列表",
-        mode = "违禁系统"
+        name="违禁词删除列表",
+        usage="违禁词删除列表",
+        permission="owner",
+        description="查看违禁词删除列表",
+        mode="违禁系统"
     )
     def dvWj(self):
         uid = self.data.se.get('user_id')
         gid = self.data.se.get('group_id')
-        
-        vKwList = self.mysql.selectx('SELECT * FROM `botWeijin` WHERE `state`=3')
-        message = '[CQ:face,id=151] {0}-违禁词删除列表'.format(self.data.botSettings.get('name'))
+
+        vKwList = BanWordsModel()._get(state=3)
+        message = '[CQ:face,id=151] {0}-违禁词删除列表'.format(self.data.botSettings._get('name'))
         for i in vKwList:
-            message += '\n[CQ:face,id=54] 违禁词：'+str(i.get('content'))+'\n      ID：'+str(i.get('id'))
+            message += '\n[CQ:face,id=54] 违禁词：' + str(i.get('content')) + '\n      ID：' + str(i.get('id'))
         self.client.msg().raw(message)
-    
+
     @RegCmd(
-        name = "违禁词审查 ",
-        usage = "违禁词审查 <ID> <是否通过>",
-        permission = "owner",
-        description = "审核违禁词",
-        mode = "违禁系统"
+        name="违禁词审查 ",
+        usage="违禁词审查 <ID> <是否通过>",
+        permission="owner",
+        description="审核违禁词",
+        mode="违禁系统"
     )
     def tWj(self):
         uid = self.data.se.get('user_id')
         gid = self.data.se.get('group_id')
         message = self.data.message
-        
+
         message1 = message.split(' ')
         kwid = message1[0]
         iff = message1[1]
@@ -104,105 +108,99 @@ class blacklist(PBF):
         else:
             state = 2
             message = '[CQ:face,id=54] 已移至回收站！'
-        self.mysql.commonx('UPDATE `botWeijin` SET `state`=%s WHERE `id`=%s', (state, kwid))
-        
+        BanWordsModel()._set({"id":kwid}, state=state)
+
         self.client.msg().raw(message)
-        
-        refreshFromSql('botWeijin')
-        
-    
+
     @RegCmd(
-        name = "删违禁词 ",
-        usage = "删违禁词 <违禁词内容>",
-        permission = "anyone",
-        description = "删除指定的违禁词",
-        mode = "违禁系统"
+        name="删违禁词 ",
+        usage="删违禁词 <违禁词内容>",
+        permission="anyone",
+        description="删除指定的违禁词",
+        mode="违禁系统"
     )
     def delWeijin(self):
         uid = self.data.se.get('user_id')
         gid = self.data.se.get('group_id')
         message = self.data.message
-        
-        if uid == self.data.botSettings.get('owner'):
-            sql = 'UPDATE `botWeijin` SET `state`=2 WHERE `content`=%s and `qn`=0'
+
+        if uid == self.data.botSettings._get('owner'):
+            BanWordsModel()._set({"content":message, "qn":0}, state=2)
         else:
-            sql = 'UPDATE `botWeijin` SET `state`=3 WHERE `content`=%s and `qn`=0'
-        self.mysql.commonx(sql, (message))
-        
+            BanWordsModel()._set({"content":message, "qn":0}, state=3)
+
         self.client.msg().raw('[CQ:face,id=54] 已提交申请，等待审核！')
-        
-        refreshFromSql('botWeijin')
-    
+
     @RegCmd(
-        name = "删群违禁词 ",
-        usage = "删群违禁词 <违禁词内容>",
-        permission = "ao",
-        description = "删除指定的群聊违禁词",
-        mode = "违禁系统"
+        name="删群违禁词 ",
+        usage="删群违禁词 <违禁词内容>",
+        permission="ao",
+        description="删除指定的群聊违禁词",
+        mode="违禁系统"
     )
     def delQunWeijin(self):
         uid = self.data.se.get('user_id')
         gid = self.data.se.get('group_id')
         message = self.data.message
-        
-        self.mysql.commonx('DELETE FROM `botWeijin` WHERE `content`=%s and `qn`!=0', (message))
-        
+
+        BanWordsModel()._delete(content=message)
+
         self.client.msg().raw('[CQ:face,id=54] 已删除！')
-        
-        refreshFromSql('botWeijin')
-    
+
     @RegCmd(
-        name = "全局屏蔽列表",
-        usage = "全局屏蔽列表",
-        permission = "anyone",
-        description = "列出被全局屏蔽的人",
-        mode = "全局屏蔽"
+        name="全局屏蔽列表",
+        usage="全局屏蔽列表",
+        permission="anyone",
+        description="列出被全局屏蔽的人",
+        mode="全局屏蔽"
     )
     def listQuanjing(self):
         uid = self.data.se.get('user_id')
         gid = self.data.se.get('group_id')
-        
-        message = '[CQ:face,id=189] {0}-全局拉黑列表'.format(self.data.botSettings.get('name'))
-        quanjing = self.mysql.selectx("SELECT * FROM `botQuanping`")
-        for i in quanjing:
-            message += '\n[CQ:face,id=54] 用户：'+str(i.get('qn'))+'\n     原因：'+str(i.get('reason'))
+
+        message = '[CQ:face,id=189] {0}-全局拉黑列表'.format(self.data.botSettings._get('name'))
+        # TODO 此处获取库中所有信息待处理
+        modelBase = ModelBase()
+        modelBase.db_table = 'black_list'
+        quanjing = Cache.get(modelBase._c())
+        for j in quanjing:
+            items = quanjing.get(j)
+            for l in items:
+                i = items.get(l)
+                message += '\n[CQ:face,id=54] 用户：' + str(i.get('qn')) + '\n     原因：' + str(i.get('reason'))
         self.client.msg().raw(message)
-    
+
     @RegCmd(
-        name = "删全局屏蔽 ",
-        usage = "删全局屏蔽 <要删的qq号>",
-        permission = "ro",
-        description = "删除某个被全局屏蔽的人",
-        mode = "全局屏蔽"
+        name="删全局屏蔽 ",
+        usage="删全局屏蔽 <要删的qq号>",
+        permission="ro",
+        description="删除某个被全局屏蔽的人",
+        mode="全局屏蔽"
     )
     def deleteQuanjing(self):
         uid = self.data.se.get('user_id')
         gid = self.data.se.get('group_id')
         message = self.data.message
-        
-        self.mysql.commonx("DELETE FROM `botQuanping` WHERE `uuid`=%s and `qn`=%s", (self.data.uuid, message))
-        
+
+        BlackListModel(uuid=self.data.uuid, qn=int(message))._delete()
+
         self.client.msg().raw('[CQ:face,id=54] 删除成功！')
-        
-        refreshFromSql('globalBanned')
 
     @RegCmd(
-        name = "加全局屏蔽 ",
-        usage = "加全局屏蔽 <要加的qq号> <原因>",
-        permission = "owner",
-        description = "添加某个被全局屏蔽的人",
-        mode = "全局屏蔽"
+        name="加全局屏蔽 ",
+        usage="加全局屏蔽 <要加的qq号> <原因>",
+        permission="owner",
+        description="添加某个被全局屏蔽的人",
+        mode="全局屏蔽"
     )
     def addQuanjing(self):
         uid = self.data.se.get('user_id')
         gid = self.data.se.get('group_id')
         message = self.data.message
-        
+
         message1 = message.split(' ')
         qn = message1[0]
         reason = message1[1]
-        self.mysql.commonx("INSERT INTO `botQuanping` (`qn`, `reason`, `uuid`, `time`) VALUES (%s, %s, %s, %s)", (qn, reason, self.data.uuid, time.time()))
-        
+        BlackListModel(uuid=self.data.uuid, time=time.time(), reason=reason, qn=qn)
+
         self.client.msg().raw('[CQ:face,id=54] 添加成功！')
-        
-        refreshFromSql('globalBanned')
